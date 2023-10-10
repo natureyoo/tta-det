78fb9f03ee4ff07d4ebc9383be077156e3cf7851
jayeon <jayeon.yoo@snu.ac.kr>
2023-10-10 16:54:45 +0900

    setup environments

---
+++ setup_tta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+virtualenv tta --python=python3.7
+source tta/bin/activate
+
+pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install -r requirements.txt
